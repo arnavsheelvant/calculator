a278b4b6fcf53dd697d517e3c9cde3c0a04a6ac3
Arnav Sheelvant <arnavsheelvant@Arnavs-MacBook-Air.local>
2026-02-03 09:59:22 +0530

    fixed code

--- calculator.c
+++ calculator.c
@@ -14,8 +14,6 @@ int main() {
         printf("%.2f", a * b);
     else if (op == '/')
         printf("%.2f", a / b);
-    else if (op == '%')
-        printf("%.2f", a % b);
     else if (op == '^')
         printf("%.2f", pow(a, b));
     return 0;
